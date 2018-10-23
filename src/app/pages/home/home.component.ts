ba9c631dcc5d52a38531d0a604a6288af1b23cde
eniotnam <marteantoine@gmail.com>
2018-10-23 22:27:06 +0200

    front

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomersService } from "../../services/customers.service";
+import { UsersService } from "../../services/users.service";
 
 @Component({
   selector: 'app-home',
@@ -6,8 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+ customers;
+ users;
 
-  constructor() { }
+  constructor(private customersService:CustomersService,private usersService:UsersService) {
+
+    this.customersService.getCustomers().subscribe(customers=>{
+        this.customers=customers;
+        console.log(customers);
+    })
+    
+    this.usersService.getUsers().subscribe(users=>{
+        this.users=users;
+        console.log(users);
+    })
+
+  }
 
   ngOnInit() {
   }
